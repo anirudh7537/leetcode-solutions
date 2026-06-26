478c1d041b219145716c3455f37504cf6f41a30b
Anirudh Gandhi <anirudhgandhi74@gmail.com>
2026-06-26 11:09:18 +0530

    Improved Best Time to Buy and Sell Stock solution formatting

--- greedy/best_time_to_buy_sell_stock.py
+++ greedy/best_time_to_buy_sell_stock.py
@@ -8,23 +8,15 @@ class Solution:
 
         for price in prices:
 
-            # Update minimum buying price
+          
             if price < min_price:
                 min_price = price
 
-            # Calculate current profit
             profit = price - min_price
 
-            # Update maximum profit
+          
             if profit > max_profit:
                 max_profit = profit
 
         return max_profit
 
-
-# Testing locally
-prices = [7, 1, 5, 3, 6, 4]
-
-result = Solution().maxProfit(prices)
-
-print(result)
