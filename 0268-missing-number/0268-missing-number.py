216b6ba84e6cf62fdf17ef9fa29d27367eb586aa
Anirudh Gandhi <anirudhgandhi74@gmail.com>
2026-07-03 10:54:52 +0530

    {Python3} {question no. of 206. Reverse Linked List}

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        expected = n*(n+1)//2
+
+        actual = sum(nums)
+
+        final = expected - actual
+        
+        return expected - actual
