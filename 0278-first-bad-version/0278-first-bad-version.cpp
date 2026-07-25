8027a500730a92701641db4557c988fe9c7597e1
Anirudh Gandhi <anirudhgandhi74@gmail.com>
2026-07-25 09:36:05 +0530

    2 ms (57.68%) 8 MB (42.29%) 07-25-2026 0278-first-bad-version Easy .cpp

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,16 +4,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-     int s = 1, e = n;
-    while(s<e){
-        int mid = s + (e-s)/2;
-        if(isBadVersion(mid))
-            e = mid;
-        else
-            s = mid +1;    
-     
-        }
+        return fBV(1,n);
+    }
+
+    int fBV(int s, int e){
+        if(s == e)
         return s;
-   
+
+        int mid = s+ (e-s)/2; //(s+e)/2 can be used but if s is larger than int max and e is also greater than s problem occurs so ideal solution to use to find mid is s+ (e-s)/2
+        if(isBadVersion(mid))
+            return fBV(s, mid);
+        else return fBV(mid+1,e);   
+
     }
 };
