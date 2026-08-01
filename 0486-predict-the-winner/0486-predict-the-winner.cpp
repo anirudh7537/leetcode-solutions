50fddcc759a53170a9ee41f646725e3914830750
Anirudh Gandhi <maheshwariradhey00@gmail.com>
2026-08-01 12:40:44 +0530

    0 ms (100%) 9.6 MB (85.12%) 08-01-2026 0486-predict-the-winner Medium .cpp

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+int t[22][22];
  int solve(int i, int j,vector<int>& nums ){
   if(i>j) return 0;
   if(i==j) return nums[i];
+  if(t[i][j]!= -1)
+  return t[i][j];
   int take_i = nums[i] + min(solve(i+2,j,nums), solve(i+1,j-1,nums));
   int take_j = nums[j] + min(solve(i+1,j-1,nums), solve(i,j-2,nums));
-  return max(take_i,take_j);
+  return t[i][j] = max(take_i,take_j);
 
  }
     bool predictTheWinner(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
         int n = nums.size();
         int totalScore = accumulate(begin(nums),end(nums), 0);
         int player1_score = solve(0,n-1,nums);
